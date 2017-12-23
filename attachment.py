d8c6e44b82566472a1f14299fcf029d8e2c8dd1d
Kevin Yokley <kyokley2@gmail.com>
2017-12-23 13:30:03 -0600

    Extracting .py and .txt files

--- attachment.py
+++ attachment.py
@@ -1,14 +1,21 @@
 import os
+import tarfile
 
 TAR_EXTENSION = '.tar.gz'
 ZIP_EXTENSION = '.zip'
+PY_EXTENSION = '.py'
+TXT_EXTENSION = '.txt'
 
-ALLOWED_EXTENSIONS = (TAR_EXTENSION,
-                      ZIP_EXTENSION,
-                      )
+ALLOWED_ARCHIVE_EXTENSIONS = (TAR_EXTENSION,
+                              ZIP_EXTENSION,
+                              )
+
+ALLOWED_CODE_EXTENSIONS = (PY_EXTENSION,
+                           TXT_EXTENSION,
+                           )
 
 def archive_extension(filename):
-    for extension in ALLOWED_EXTENSIONS:
+    for extension in ALLOWED_ARCHIVE_EXTENSIONS:
         if extension in filename:
             return extension
 
@@ -19,6 +26,16 @@ def archive_basename(filename):
 
     basename = filename.split(ext)[0]
     return basename
+
+def _tar_filter(members):
+    for tarinfo in members:
+        if ('..' in tarinfo.name or
+                tarinfo.name.startswith('/')):
+            continue
+
+        if os.path.splitext(tarinfo.name)[1] in ALLOWED_CODE_EXTENSIONS:
+            yield tarinfo
+
 
 def decompress_archives(path):
     archives = os.listdir(path)
@@ -38,4 +55,11 @@ def decompress_archives(path):
             continue
 
         os.mkdir(new_dir)
-        os.rename(full_path, os.path.join(new_dir, archive))
+
+        new_full_path = os.path.join(new_dir, archive)
+        os.rename(full_path, new_full_path)
+
+        if ext is TAR_EXTENSION:
+            tar_file = tarfile.open(new_full_path, 'r')
+            tar_file.extractall(path=new_dir, members=_tar_filter(tar_file))
+            tar_file.close()
