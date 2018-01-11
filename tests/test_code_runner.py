dd0dbba8fb1d109177b03cddcc5c68a363e65cda
Kevin Yokley <kyokley2@gmail.com>
2018-01-11 09:39:47 -0600

    Testing check_triangle

--- tests/test_code_runner.py
+++ tests/test_code_runner.py
@@ -32,8 +32,13 @@ class TestCheckTriangle(object):
         self.run_patcher = mock.patch('code_runner.run')
         self.mock_run = self.run_patcher.start()
 
+        self.abspath_patcher = mock.patch('code_runner.os.path.abspath')
+        self.mock_abspath = self.abspath_patcher.start()
+        self.mock_abspath.return_value = 'test_abspath'
+
     def teardown_method(self):
         self.run_patcher.stop()
+        self.abspath_patcher.stop()
 
     def test_no_filename_raises(self):
         with pytest.raises(StopExecution):
@@ -44,4 +49,11 @@ class TestCheckTriangle(object):
             check_triangle('test_path', filename='test_filename')
 
     def test_check(self):
-        pass
+        expected = None
+        actual = check_triangle('test_path', filename='test_filename', expected=[5])
+
+        assert expected == actual
+        self.mock_run.assert_called_once_with('test_path',
+                                              'python3 traversal.py test_abspath/data/test_filename',
+                                              expected=[5],
+                                              conversion_func=None)
